a5a0bfcd11b081a02fd97bd14dd3c915316446f8
configbast1 <stiilboimailur@gmail.com>
2025-06-28 13:51:48 +0300

    Update Reservoir.h
    Add Reservoir class definition

--- oop-cpp/ReservoirProject/ include/Reservoir.h
+++ oop-cpp/ReservoirProject/ include/Reservoir.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
 
+class Reservoir {
+private:
+    char name[50];
+    char type[50];
+    double width;
+    double length;
+    double maxDepth; 
+
+сlass Reservoir {
+private:
+    char name[50];
+    char type[50];
+    double width;
+    double length;
+    double maxDepth;
