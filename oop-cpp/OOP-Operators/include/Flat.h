d7ddb0a72cebd8ded528c0e5a192e8abe2362634
configbast1 <stiilboimailur@gmail.com>
2025-06-29 15:27:32 +0300

    Create Flat.h

---
+++ oop-cpp/OOP-Operators/include/Flat.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Flat {
+private:
+    double area;
+    double price;
+
+public:
+    Flat();
+    Flat(double a, double p);
+
+    bool operator==(const Flat& other) const;
+    Flat& operator=(const Flat& other);
+    bool operator>(const Flat& other) const;
+
+    void show() const;
+};
