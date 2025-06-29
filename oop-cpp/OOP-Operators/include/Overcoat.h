1c3be272f25b3a07e38d37b27ff3ad1c5e5d9005
configbast1 <stiilboimailur@gmail.com>
2025-06-29 15:23:14 +0300

    Update Overcoat.h

--- oop-cpp/OOP-Operators/include/Overcoat.h
+++ oop-cpp/OOP-Operators/include/Overcoat.h
@@ -1,2 +1,20 @@
+#pragma once
+#include "MyString.h" 
+
+class Overcoat {
+private:
+    char type[30];
+    double price;
+
+public:
+    Overcoat();
+    Overcoat(const char* t, double p);
+
+    bool operator==(const Overcoat& other) const;
+    Overcoat& operator=(const Overcoat& other);
+    bool operator>(const Overcoat& other) const;
+
+    void show() const;
+};
 
 
