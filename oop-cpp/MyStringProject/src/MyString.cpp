402f4ac3aa3a103c7fe78e905dbee09693097863
configbast1 <stiilboimailur@gmail.com>
2025-06-29 16:19:45 +0300

    Update MyString.cpp
    Add default and size constructors

--- oop-cpp/MyStringProject/src/MyString.cpp
+++ oop-cpp/MyStringProject/src/MyString.cpp
@@ -3,4 +3,14 @@
 
 using namespace std; 
 
+MyString::MyString() {
+    str = nullptr;
+    length = 0;
+}
 
+
+MyString::MyString(int size) {
+    length = size;
+    str = new char[length + 1];
+    str[0] = '\0';
+}
