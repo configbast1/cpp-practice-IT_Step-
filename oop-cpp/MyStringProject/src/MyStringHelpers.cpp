5a40ba370ab18582090892a7188e8077d9a62d5a
configbast1 <stiilboimailur@gmail.com>
2025-06-29 16:33:29 +0300

    Update MyStringHelpers.cpp
    Add mystrcpy function

--- oop-cpp/MyStringProject/src/MyStringHelpers.cpp
+++ oop-cpp/MyStringProject/src/MyStringHelpers.cpp
@@ -5,3 +5,16 @@ int mystrlen(const char* s) {
     while (*s++) ++len;
     return len;
 }
+
+int mystrlen(const char* s) {
+    int len = 0;
+    while (*s++) ++len;
+    return len;
+}
+
+void mystrcpy(char* dest, const char* src) {
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
