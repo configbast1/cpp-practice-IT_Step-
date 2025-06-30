0ca984a3b225c2713139fb715078ea7f44eab7cb
configbast1 <stiilboimailur@gmail.com>
2025-06-30 23:05:35 +0300

    Update Array.h

--- oop-cpp/ArrayTemplateProject/ Array/Array.h
+++ oop-cpp/ArrayTemplateProject/ Array/Array.h
@@ -2,3 +2,17 @@
 
 #include <iostream>
 using namespace std;
+
+template <class T>
+class Array {
+private:
+    T* data;           
+    int size;         
+    int grow;          
+public:
+    // Конструктор по умолчанию
+    Array() {
+        data = nullptr;
+        size = 0;
+        grow = 1;  
+    }
