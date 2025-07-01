20b6b54e426714927c907de4644447503505a97d
configbast1 <stiilboimailur@gmail.com>
2025-07-01 22:59:52 +0300

    Update Task1_Print0toN.cpp

--- LoopsTasks/Task1_Print0toN.cpp
+++ LoopsTasks/Task1_Print0toN.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    int n;
+    cout << "Введіть число: ";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
