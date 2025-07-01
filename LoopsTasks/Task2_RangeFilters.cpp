d83ff381d2cc190afd6ea49cff068ef30d59f3f1
configbast1 <stiilboimailur@gmail.com>
2025-07-01 23:01:10 +0300

    Update Task2_RangeFilters.cpp

--- LoopsTasks/Task2_RangeFilters.cpp
+++ LoopsTasks/Task2_RangeFilters.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b;
+    cout << "Введіть дві межі діапазону: ";
+    cin >> a >> b;
+
+    int start = (a < b) ? a : b;
+    int end = (a > b) ? a : b;
+
+    cout << "Усі числа: ";
+    for (int i = start; i <= end; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
