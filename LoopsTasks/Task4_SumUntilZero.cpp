29c4986f06b0d1a7ebc92c2b54a1562951e03cbb
configbast1 <stiilboimailur@gmail.com>
2025-07-01 23:02:57 +0300

    Update Task4_SumUntilZero.cpp

--- LoopsTasks/Task4_SumUntilZero.cpp
+++ LoopsTasks/Task4_SumUntilZero.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int main() {
     int num;
     int sum = 0;
+
+ cout << "Вводьте числа (0 для завершення): " << endl;
+
+    do {
+        cin >> num;
+        sum += num;
+    } while (num != 0);
+
+    cout << "Сума введених чисел: " << sum << endl;
+
+    return 0;
+}
