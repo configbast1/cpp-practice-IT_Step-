d581d289fce41afba7a72053ef59f9509efd03bf
configbast1 <stiilboimailur@gmail.com>
2025-07-03 00:53:47 +0300

    Update main.cpp

--- oop-cpp/OOPTasks/StringBitTask/main.cpp
+++ oop-cpp/OOPTasks/StringBitTask/main.cpp
@@ -12,4 +12,24 @@ String str3(str2);
     str3.print();
 
 str1 = str2;
-    str1.print(); 
+    str1.print();  
+    
+std::cout << "Length: " << str2.length() << std::endl;
+    str2.clear();
+    str2.print();
+
+    String str4("World");
+    String str5 = str3 + str4;
+    str5.print();
+
+    str3 += str4;
+    str3.print();
+
+    if (str3 == str5) {
+        std::cout << "Strings are equal!" << std::endl;
+    } else {
+        std::cout << "Strings are NOT equal!" << std::endl;
+    }
+
+    return 0;
+}
