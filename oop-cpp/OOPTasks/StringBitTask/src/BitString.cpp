9cac2ce4693ef0415b91329734b42c88db485a1c
configbast1 <stiilboimailur@gmail.com>
2025-07-03 01:11:43 +0300

    Update BitString.cpp
    BitString constructor with C-string

--- oop-cpp/OOPTasks/StringBitTask/src/BitString.cpp
+++ oop-cpp/OOPTasks/StringBitTask/src/BitString.cpp
@@ -2,3 +2,20 @@
 #include <cstring> 
 
 BitString::BitString() : String() {} 
+
+BitString::BitString(const char* str) : String() {
+    bool valid = true;
+    for (size_t i = 0; i < strlen(str); ++i) {
+        if (str[i] != '0' && str[i] != '1') {
+            valid = false;
+            break;
+        }
+    }
+    if (valid) {
+        delete[] data;
+        data = new char[strlen(str) + 1];
+        strcpy(data, str);
+    } else {
+        clear();
+    }
+}
