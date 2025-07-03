1fd30b5a19f3d13674a117c3a403d87658ab11cb
configbast1 <stiilboimailur@gmail.com>
2025-07-03 15:36:11 +0300

    Update Dog.h
    Implement Dog methods

--- oop-cpp/oop-inheritance/PetProject/include/Dog.h
+++ oop-cpp/oop-inheritance/PetProject/include/Dog.h
@@ -5,3 +5,11 @@
 
 #include "Pet.h"
 #include <iostream>
+
+class Dog : public Pet {
+public:
+    Dog(const string &name) : Pet(name) {}
+    void Show() const override;
+    void Sound() const override;
+    void Type() const override;
+};
