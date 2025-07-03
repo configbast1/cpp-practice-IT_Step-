1340fc04f9ab209dfb3f8531fed4e9236e705925
configbast1 <stiilboimailur@gmail.com>
2025-07-03 14:24:12 +0300

    Update BitString.h
     Add BitString destructor, IsValid, InvertSign, operator+

--- oop-cpp/oop-inheritance/include/BitString.h
+++ oop-cpp/oop-inheritance/include/BitString.h
@@ -21,3 +21,24 @@ public:
         }
         return *this;
     }
+
+ ~BitString() {}
+
+    bool IsValid() const {
+        for (int i = 0; i < length; ++i) {
+            if (str[i] != '0' && str[i] != '1') return false;
+        }
+        return true;
+    }
+
+    void InvertSign() {
+        for (int i = 0; i < length; ++i) {
+            str[i] = (str[i] == '0') ? '1' : '0';
+        }
+    }
+
+    BitString operator+(const BitString& other) const {
+        BitString result(*this);
+        result += other;
+        return result;
+    }
