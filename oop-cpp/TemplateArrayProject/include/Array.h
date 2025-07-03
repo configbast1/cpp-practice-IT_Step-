062165199a013b436786b7d5fe7314fe82e73d8b
configbast1 <stiilboimailur@gmail.com>
2025-07-03 13:08:29 +0300

    Update Array.h
    Step 2: Add fields, constructor, destructor

--- oop-cpp/TemplateArrayProject/include/Array.h
+++ oop-cpp/TemplateArrayProject/include/Array.h
@@ -3,7 +3,25 @@
 
 template <typename T>
 class Array {
+private:
+    T* data;
+    int size;
+    int grow;
+    int count;
 
+public:
+    Array(int initSize = 0, int growSize = 1)
+        : size(initSize), grow(growSize), count(0)
+    {
+        if (size > 0)
+            data = new T[size];
+        else
+            data = nullptr;
+    }
+
+    ~Array() {
+        delete[] data;
+    }
 
 
 };
