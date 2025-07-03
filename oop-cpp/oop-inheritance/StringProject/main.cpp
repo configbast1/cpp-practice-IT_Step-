4d6e86493eb8c458d8e73f64e0c9fd673cb1b087
configbast1 <stiilboimailur@gmail.com>
2025-07-03 14:53:53 +0300

    Update main.cpp
    Add String test: create, concat and compare

--- oop-cpp/oop-inheritance/StringProject/main.cpp
+++ oop-cpp/oop-inheritance/StringProject/main.cpp
@@ -2,3 +2,17 @@
 #include "include/BitString.h"
 
 int main() {
+
+ String animal1("Dog");
+    String animal2("Cat");
+    String combined = animal1 + animal2;
+
+    animal1.Show();
+    animal2.Show();
+    combined.Show();
+
+    if (animal1 == animal2) {
+        std::cout << "Equal\n";
+    } else {
+        std::cout << "Not equal\n";
+    }
