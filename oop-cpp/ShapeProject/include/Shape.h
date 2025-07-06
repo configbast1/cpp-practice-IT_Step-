1b71f3be219b4e34be53b47a3e3984c17b56ab6a
configbast1 <stiilboimailur@gmail.com>
2025-07-06 23:25:48 +0300

    Update Shape.h
    Shape

--- oop-cpp/ShapeProject/include/Shape.h
+++ oop-cpp/ShapeProject/include/Shape.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+class Shape {
+public:
+    virtual void Show() = 0;
+    virtual void Save(ofstream& fout) = 0;
+    virtual void Load(ifstream& fin) = 0;
+
+    virtual ~Shape() {}
+};
