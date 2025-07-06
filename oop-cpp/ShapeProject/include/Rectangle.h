f14fd4eb0758d7c90f4280b768b2e3acb4e81261
configbast1 <stiilboimailur@gmail.com>
2025-07-06 23:34:03 +0300

    Update Rectangle.h
    create rectangle.h file

--- oop-cpp/ShapeProject/include/Rectangle.h
+++ oop-cpp/ShapeProject/include/Rectangle.h
@@ -1,1 +1,15 @@
+#pragma once
+#include "Shape.h"
 
+class Rectangle : public Shape {
+private:
+    int x, y;
+    int width, height;
+public:
+    Rectangle();
+    Rectangle(int _x, int _y, int _w, int _h);
+
+    void Show() override;
+    void Save(ofstream& fout) override;
+    void Load(ifstream& fin) override;
+};
