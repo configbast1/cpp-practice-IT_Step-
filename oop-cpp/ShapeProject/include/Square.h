458da19d4cc238d7c5afa7c4f44b153ec8d12929
configbast1 <stiilboimailur@gmail.com>
2025-07-06 23:31:46 +0300

    Update Square.h
    create square.h file

--- oop-cpp/ShapeProject/include/Square.h
+++ oop-cpp/ShapeProject/include/Square.h
@@ -1,1 +1,15 @@
+#pragma once
+#include "Shape.h"
 
+class Square : public Shape {
+private:
+    int x, y;
+    int side;
+public:
+    Square();
+    Square(int _x, int _y, int _side);
+
+    void Show() override;
+    void Save(ofstream& fout) override;
+    void Load(ifstream& fin) override;
+};
