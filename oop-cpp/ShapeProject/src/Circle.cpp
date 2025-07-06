372adb38fbb66786666de28672a53800afae4e31
configbast1 <stiilboimailur@gmail.com>
2025-07-06 23:47:21 +0300

    Update Circle.cpp
    Add Circle.cpp with implementation of Circle class methods

--- oop-cpp/ShapeProject/src/Circle.cpp
+++ oop-cpp/ShapeProject/src/Circle.cpp
@@ -1,1 +1,18 @@
+#include "../include/Circle.h"
+
+Circle::Circle() : x(0), y(0), radius(0) {}
+
+Circle::Circle(int _x, int _y, int _r) : x(_x), y(_y), radius(_r) {}
+
+void Circle::Show() {
+    cout << "Круг: центр (" << x << ", " << y << "), радиус = " << radius << endl;
+}
+
+void Circle::Save(ofstream& fout) {
+    fout << "Circle " << x << " " << y << " " << radius << endl;
+}
+
+void Circle::Load(ifstream& fin) {
+    fin >> x >> y >> radius;
+}
 
