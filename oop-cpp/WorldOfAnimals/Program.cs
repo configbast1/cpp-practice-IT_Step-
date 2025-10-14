5fd4469fe13aaeddbe2b6147007b5e87aae45674
configbast1 <stiilboimailur@gmail.com>
2025-10-14 15:25:55 +0200

    Update Program.cs

--- oop-cpp/WorldOfAnimals/Program.cs
+++ oop-cpp/WorldOfAnimals/Program.cs
@@ -1,1 +1,14 @@
+using WorldOfAnimals;
+using WorldOfAnimals.Continents;
 
+class Program
+{
+    static void Main()
+    {
+        var world = new World();
+        world.AddContinent(new Africa());
+        world.AddContinent(new NorthAmerica());
+
+        world.Simulate();
+    }
+}
