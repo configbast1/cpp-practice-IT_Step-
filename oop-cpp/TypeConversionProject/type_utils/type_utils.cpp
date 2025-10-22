ac91cf04cb574c87be0eb15df400c91cfd906f5b
configbast1 <stiilboimailur@gmail.com>
2025-10-22 14:35:13 +0200

    Update type_utils.cpp

--- oop-cpp/TypeConversionProject/type_utils/type_utils.cpp
+++ oop-cpp/TypeConversionProject/type_utils/type_utils.cpp
@@ -1,7 +1,20 @@
 #include "type_utils.h"
-namespace ctd {
-void printCharFromInt(int number) {
-    char* chPtr = reinterpret_cast<char*>(&number);
-    std::cout << "Символ из числа: " << *chPtr << std::endl;
+
+using namespace std;
+namespace type_utils {
+
+template<typename T>
+void printTypeName(const T& value) {
+    cout << "Type: " << typeid(value).name() << endl;
 }
-} 
+template void printTypeName<int>(const int&);
+template void printTypeName<double>(const double&);
+template void printTypeName<char>(const char&);
+template void printTypeName<string>(const string&);
+
+void printCharFromInt(int number) {
+    char c = *reinterpret_cast<char*>(&number);
+    cout << "Char from int (" << number << "): " << c << endl;
+}
+
+}
