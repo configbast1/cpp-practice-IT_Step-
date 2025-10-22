d82dbd628a1b4da9cd62cc00cb4890d793404aa4
configbast1 <stiilboimailur@gmail.com>
2025-10-22 14:26:49 +0200

    Update type_utils.h

--- oop-cpp/TypeConversionProject/type_utils/type_utils.h
+++ oop-cpp/TypeConversionProject/type_utils/type_utils.h
@@ -1,8 +1,12 @@
 #ifndef TYPE_UTILS_H
 #define TYPE_UTILS_H
+#include <iostream>
+#include <typeinfo>
 namespace ctd {
 template<typename T>
-void printTypeName(const T& value);
+void printTypeName(const T& value) {
+    std::cout << "Имя типа: " << typeid(value).name() << std::endl;
+}
 void printCharFromInt(int number);
 } 
 #endif 
